8b6736e065f752e12111a78ae086a58e6de2f108
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-05-23 19:53:04 +0300

    Added the new schema for lyrics

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,6 +1,14 @@
-CREATE TABLE IF NOT EXISTS scrape.News (
-    "Headline" TEXT,
-    "Content" TEXT,
-    "Source" TEXT,
-    "date" TEXT
+--CREATE TABLE IF NOT EXISTS scrape.News (
+  --  "Headline" TEXT,
+   -- "Content" TEXT,
+    --"Source" TEXT,
+    --"date" TEXT
+--);
+
+
+CREATE TABLE IF NOT EXISTS scrape.Lyrics (
+   "artist" VARCHAR(255),
+    "song_name" VARCHAR(255),
+    "lyrics" TEXT,
+    "duration" VARCHAR(50)
 );
