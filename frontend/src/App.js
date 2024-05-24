e948df852274ed25515000820369fc249aabd6c5
mistirnigusse <mistirnigusse00@gmail.com>
2024-05-24 22:06:43 +0300

    Feat: created the necessary components and set up route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+// src/App.js
+import React from 'react';
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-function App() {
+import Home from './pages/home';
+import DataView from './pages/DataView';
+import routes from './routes';
+
+const App = () => {
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Routes>
+                {getRoutes(routes)}
+
+        <Route path="*"  component={Home} />
+
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+
+import Home from "./pages/home";
+import DataView from "./pages/DataView";
+
+const routes = [
+  {
+  
+    route: "/",
+    component: <Home />,
+  },
+  {
+
+    route: "/detail",
+    component: <DataView/>,
+  }
+];
+
+export default routes;
